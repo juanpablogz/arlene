b924a707b85221b10bb1bdc5bb0e5aabc94037ec
juan pablo gomez zapata <juanpablogomezzapata@gmail.com>
2021-10-11 17:25:24 -0300

    initialize challenge && config tailwind

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="absolute bottom-0 left-0">Lorem ipsum dolor sit amet...</div>
         <a
           className="App-link"
           href="https://reactjs.org"
