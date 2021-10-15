e04b20015e77f0412bf4ca9421231837609ca86c
juan pablo gomez zapata <juanpablogomezzapata@gmail.com>
2021-10-15 12:36:29 -0300

    add env example && readme

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -11,7 +11,6 @@ import {
   signOut,
 } from "firebase/auth";
 const UserContext = createContext();
-console.log(process.env)
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
